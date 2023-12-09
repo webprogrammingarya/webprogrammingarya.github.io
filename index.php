34e4a64f272a1885f80f06cba27176b001a27e3e
Arya Dimas <webprogrammingarya>
2023-12-09 12:15:17 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,177 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arya Dimas Wicaksana - Portfolio</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>
+        p {
+            font-family: monospace;
+        }
+        .img-icon {
+            height: 50px;
+            filter: grayscale(1)
+        }
+        .img-project {
+            filter: grayscale(1);
+            cursor: pointer;
+        }
+        .img-icon:hover, .img-project:hover {
+            filter: grayscale(0)
+        }
+        .nav-social-media {
+            background: red;
+        }
+        .social-media {
+            font-size: 2rem;
+            border-radius: 100%;
+        }
+        .social-media a {
+            color: black;
+        }
+        .social-media a:hover {
+            border-radius: 100%;
+            background-color: black;
+            padding: 10px;
+            font-size: 2.2rem;
+            color: white;
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+        }
+    </style>
+</head>
+<body>
+
+    <hr class="border border-black opacity-100 mt-5">
+
+    <div class="container my-5">
+        <div class="row">
+            <div class="col-md-4">
+                <h2>ADW</h2> 
+            </div>
+            <div class="col-md-8">
+                <a href="#" class="text-decoration-none text-black">ABOUT</a>
+            </div>
+        </div>
+    </div>
+
+    <hr class="border border-black opacity-100 mb-0">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-10 border-end border-black opacity-100">
+                <div class="row mb-5">
+                    <div class="col-lg-4 text-center">
+                        <img src="images/me.jpg" alt="" class="img-fluid img-profile">
+                    </div>
+                    <div class="col-lg-8 d-flex align-items-center">
+                        <p class="ps-4">
+                            Hello👋! <br>
+        
+                            <br>My name is Arya Dimas Wicaksana, and I am a student in the Software Engineering Technology program at the IPB University Vocational School.<br>
+        
+                            <br>I have a strong interest in web development. I am currently in the process of improving my web development skills and will continue to learn. I hope I can follow in the footsteps of my role model “Sandhika Galih”, who is a lecturer, content creator and developer.
+                        </p>
+                    </div>
+                </div>
+        
+                <section class="mb-5">
+                    <h2 class="text-center mb-4">Educational Background</h2>
+                    <ul>
+                        <li><p>
+                                <b>2022 - Now</b> <br>
+                                IPB University - Undergraduate of Software Engineering Technology
+                            </p>
+                        </li>
+                        <li><p>
+                                <b>2019 - 2022</b> <br>
+                                SMK Prestasi Prima - Software Engineering
+                            </p>
+                        </li>
+                    </ul>
+                </section>
+        
+                <section class="mb-5">
+                    <h2 class="text-center mb-4">Skills</h2>
+                    <p>Web Development</p>
+                    <ul>
+                        <li>Language Programming <br> 
+                            <div class="d-flex my-2">
+                                <img src="images/js.png" alt="" class="img-icon me-3">
+                                <img src="images/php.png" alt="" class="img-icon me-3">
+                            </div>
+                            <ul>
+                                <li> Frontend <br>
+                                    <div class="d-flex my-2">
+                                        <img src="images/html-5.png" alt="" class="img-icon me-3">
+                                        <img src="images/css-3.png" alt="" class="img-icon me-3">
+                                        <img src="images/bootstrap.png" alt="" class="img-icon me-3">
+                                        <img src="images/jquerys.png" alt="" class="img-icon me-3">
+                                    </div>
+                                </li>
+                                <li> Backend <br> 
+                                    <div class="d-flex my-2">
+                                        <img src="images/ci.png" alt="" class="img-icon me-3">
+                                    </div>
+                                </li>
+                                <li> Database <br>
+                                    <div class="d-flex my-2">
+                                        <img src="images/mysql.png" alt="" class="img-icon me-3">
+                                        <img src="images/postgresql.png" alt="" class="img-icon me-3">
+                                        <img src="images/mongodbs.svg" alt="" class="img-icon me-3">
+                                    </div>
+                                </li>
+                                <li> VCS <br>
+                                    <div class="d-flex my-2">
+                                        <img src="images/github.png" alt="" class="img-icon me-3">
+                                        <img src="images/git.png" alt="" class="img-icon me-3">
+                                    </div>
+                                </li>
+                                <li> CMS <br>
+                                    <div class="d-flex mt-2">
+                                        <img src="images/wordpress.png" alt="" class="img-icon me-3">
+                                    </div>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </section>
+
+                <section class="mb-5">
+                    <h2 class="text-center mb-4">Project</h2>
+
+                    <a href="https://leleque.online/" target="_blank">
+                        <img src="images/leleque.PNG" alt="" class="img-fluid img-project">
+                        <p class="small mt-2 text-center text-decoration-none text-black link-project"><i>leleque – catfish financial management application</i></p>
+                    </a>
+                </section>
+            </div>
+            <div class="col-2 mt-3">
+                <p><b><u>Social Media</u></b></p>
+                <div class="d-flex justify-content-around social-media">
+                    <a href="#">
+                        <i class="fa-brands fa-instagram"></i>
+                    </a>
+                    <a href="#">
+                        <i class="fa-brands fa-linkedin"></i>
+                    </a>
+                    <a href="#">
+                        <i class="fa-brands fa-github"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <hr class="border border-black opacity-100 m-0 mb-5">
+
+    <div class="container">
+        <p>&copy; Copyright Arya Dimas Wicaksana <?= date('Y') ?></p>
+    </div>
+    
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
